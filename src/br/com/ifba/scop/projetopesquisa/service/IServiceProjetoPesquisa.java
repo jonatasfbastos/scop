b4402336046121ff5c8ae95033dfeba7bafcd7b7
Itallo007 <dipirona@dipirona-HP-EliteBook-8470p>
2020-01-10 20:49:18 -0300

    Cria os métodos da interface IServiceProjetoPesquisa
    criação dos métodos abstratos da interface IServiceProjetoPesquisa e a
    sobrescrição dos mesmos na classe ServiceProjetoPesquisa; Obs: Os 
    métodos ainda não foram implementados

--- src/br/com/ifba/scop/projetopesquisa/service/IServiceProjetoPesquisa.java
+++ src/br/com/ifba/scop/projetopesquisa/service/IServiceProjetoPesquisa.java
@@ -5,10 +5,26 @@
  */
 package br.com.ifba.scop.projetopesquisa.service;
 
+import br.com.ifba.scop.projetopesquisa.model.ProjetoPesquisa;
+import java.util.List;
+
 /**
  *
  * @author jonatasfbastos
  */
 public interface IServiceProjetoPesquisa {
+    
+    // Metodo que salva um Projeto de Pesquisa na base de dados
+    public abstract ProjetoPesquisa saveProjetoPesquisa (ProjetoPesquisa projetoPesquisa);
+    // Metodo que atualiza um Projeto de Pesquisa que já existe na base de dados
+    public abstract ProjetoPesquisa updateProjetoPesquisa (ProjetoPesquisa projetoPesquisa);
+    // Metodo que deleta um Projeto de Pesquisa da base de dados
+    public abstract void deleteProjetoPesquisa (ProjetoPesquisa projetoPesquisa);
+    // Metodo que retorna todos os Projeto de Pesquisa da base de dados
+    public abstract List<ProjetoPesquisa> getAll();
+    // Metodo que busca um Projeto de Pesquisa na base de dados através do nome
+    public abstract ProjetoPesquisa findByProjetoTitulo (ProjetoPesquisa projetoPesquisa);
+    // Metodo que verifica se o Projeto de Pesquisa já existe na base de dados
+    public abstract boolean jaExiste (ProjetoPesquisa projetoPesquisa);
     
 }
--- src/br/com/ifba/scop/projetopesquisa/service/ServiceProjetoPesquisa.java
+++ src/br/com/ifba/scop/projetopesquisa/service/ServiceProjetoPesquisa.java
@@ -6,12 +6,43 @@
 package br.com.ifba.scop.projetopesquisa.service;
 
 import br.com.ifba.scop.projetopesquisa.model.ProjetoPesquisa;
+import java.util.List;
 
 /**
  *
  * @author jonatasfbastos
  */
 public class ServiceProjetoPesquisa implements IServiceProjetoPesquisa{
+
+    @Override
+    public ProjetoPesquisa saveProjetoPesquisa(ProjetoPesquisa projetoPesquisa) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public ProjetoPesquisa updateProjetoPesquisa(ProjetoPesquisa projetoPesquisa) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void deleteProjetoPesquisa(ProjetoPesquisa projetoPesquisa) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public List<ProjetoPesquisa> getAll() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public ProjetoPesquisa findByProjetoTitulo(ProjetoPesquisa projetoPesquisa) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean jaExiste(ProjetoPesquisa projetoPesquisa) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
     
        
 }
