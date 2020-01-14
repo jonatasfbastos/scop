7911904ef5de3790eefd03a652e2fb18c05a70fc
itallo007 <itallo007@itallo007-HP-EliteBook-8470p>
2020-01-13 22:45:45 -0300

    Adiciona os métodos da IServiceProjetoPesquisa à Fachada
    os métodos foram adicionados à IFachada e implementados na Fachada

--- src/br/com/ifba/scop/infraestructure/service/Fachada.java
+++ src/br/com/ifba/scop/infraestructure/service/Fachada.java
@@ -1,9 +1,53 @@
 package br.com.ifba.scop.infraestructure.service;
 
+import br.com.ifba.scop.projetopesquisa.model.ProjetoPesquisa;
+import br.com.ifba.scop.projetopesquisa.service.IServiceProjetoPesquisa;
+import br.com.ifba.scop.projetopesquisa.service.ServiceProjetoPesquisa;
+import java.util.List;
+
 
 public class Fachada implements IFachada {
+    
+    //------------PROJETO PESQUISA-------------
+    
+    private final IServiceProjetoPesquisa serviceProjeto = new ServiceProjetoPesquisa();
+    
+    @Override
+    public ProjetoPesquisa saveProjetoPesquisa(ProjetoPesquisa projetoPesquisa) {
+        return this.serviceProjeto.saveProjetoPesquisa(projetoPesquisa);
+    }
 
-//------------PROJETO PESQUISA-------------
+    @Override
+    public ProjetoPesquisa updateProjetoPesquisa(ProjetoPesquisa projetoPesquisa) {
+        return this.serviceProjeto.updateProjetoPesquisa(projetoPesquisa);
+    }
+
+    @Override
+    public void deleteProjetoPesquisa(ProjetoPesquisa projetoPesquisa) {
+        this.serviceProjeto.deleteProjetoPesquisa(projetoPesquisa);
+    }
+
+    @Override
+    public List<ProjetoPesquisa> getAll() {
+        return this.serviceProjeto.getAll();
+    }
+
+    @Override
+    public ProjetoPesquisa findByTitulo(ProjetoPesquisa projetoPesquisa) {
+        return this.serviceProjeto.findByTitulo(projetoPesquisa);
+    }
+
+    @Override
+    public boolean projetoExistente(ProjetoPesquisa projetoPesquisa) {
+        return this.serviceProjeto.projetoExistente(projetoPesquisa);
+    }
+
+    @Override
+    public boolean validaProjetoPesquisa(ProjetoPesquisa projetoPesquisa) {
+        return this.serviceProjeto.validaProjetoPesquisa(projetoPesquisa);
+    }
+
+
     
     
 	
--- src/br/com/ifba/scop/infraestructure/service/IFachada.java
+++ src/br/com/ifba/scop/infraestructure/service/IFachada.java
@@ -1,6 +1,26 @@
 package br.com.ifba.scop.infraestructure.service;
 
+import br.com.ifba.scop.projetopesquisa.model.ProjetoPesquisa;
+import java.util.List;
+
 
 public interface IFachada {
-	
+    
+    //------------- ProjetoPesquisa ----------------------
+    
+    // Metodo que salva um Projeto de Pesquisa na base de dados
+    public abstract ProjetoPesquisa saveProjetoPesquisa (ProjetoPesquisa projetoPesquisa);
+    // Metodo que atualiza um Projeto de Pesquisa que já existe na base de dados
+    public abstract ProjetoPesquisa updateProjetoPesquisa (ProjetoPesquisa projetoPesquisa);
+    // Metodo que deleta um Projeto de Pesquisa da base de dados
+    public abstract void deleteProjetoPesquisa (ProjetoPesquisa projetoPesquisa);
+    // Metodo que retorna todos os Projeto de Pesquisa da base de dados
+    public abstract List<ProjetoPesquisa> getAll();
+    // Metodo que busca um Projeto de Pesquisa na base de dados através do titulo
+    public abstract ProjetoPesquisa findByTitulo (ProjetoPesquisa projetoPesquisa);
+    // Metodo que verifica se o Projeto de Pesquisa já existe na base de dados
+    public abstract boolean projetoExistente (ProjetoPesquisa projetoPesquisa);
+    // Metodo que valida o Projeto de Pesquisa 
+    public abstract boolean validaProjetoPesquisa (ProjetoPesquisa projetoPesquisa);
+    
 }
