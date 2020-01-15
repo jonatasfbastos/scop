3e88091c7dd5f61b70f46f542bb0796d42a014c4
Abelcoder754 <abelbarretodecarvalho@gmail.com>
2020-01-14 21:45:13 -0300

    Service de deletar, atualizar, cadastrar parcialmente prontos

--- src/br/com/ifba/scop/patente/service/ServicePatente.java
+++ src/br/com/ifba/scop/patente/service/ServicePatente.java
@@ -13,6 +13,11 @@ import br.com.ifba.scop.infraestructure.support.StringUtil;
  * @author abel
  */
 public class ServicePatente implements IServicePatente {
+    private boolean success;
+    
+    public ServicePatente() {
+        this.success = false;
+    }
     
     /**
      * Option for validate data string.
@@ -27,16 +32,68 @@ public class ServicePatente implements IServicePatente {
      */
     @Override
     public void savePatente(Patente patente) {
-        
+        this.setSuccess(true);
+        // numero da patente, se negativo
+        if (patente.getNumero() < 1) {
+            this.setSuccess(false);
+            return;
+        }
+        // titulo invenção - testa - vazio ou nulo
+        if (StringUtil.getInstance().isEmpty(patente.getTituloInvencao())) {
+            this.success = false;
+            return;
+        }
+        if (StringUtil.getInstance().isNull(patente.getTituloInvencao())) {
+            this.setSuccess(false);
+            return;
+        }
+        // area invenção - testa - vazio ou nulo
+        if (StringUtil.getInstance().isEmpty(patente.getAreaInvencao())) {
+            this.setSuccess(false);
+            return;
+        }
+        if (StringUtil.getInstance().isNull(patente.getAreaInvencao())) {
+            this.setSuccess(false);
+            return;
+        }
+        // se chegou até aqui, dados aprovados
     }
 
     /**
-     * Valida dados para fazer atualização.
+     * Valida dados para fazer atualização - pelo número da patente.
      * @param patente Objeto Patente.
      */
     @Override
     public void updatePatente(Patente patente) {
-        
+        this.setSuccess(true);
+        // numero da patente, se negativo 
+        if (patente.getId() < 1) {
+            this.setSuccess(false);
+            return;
+        }
+        if (patente.getNumero() < 1) {
+            this.setSuccess(false);
+            return;
+        }
+        // titulo invenção - testa - vazio ou nulo
+        if (StringUtil.getInstance().isEmpty(patente.getTituloInvencao())) {
+            this.success = false;
+            return;
+        }
+        if (StringUtil.getInstance().isNull(patente.getTituloInvencao())) {
+            this.setSuccess(false);
+            return;
+        }
+        // area invenção - testa - vazio ou nulo
+        if (StringUtil.getInstance().isEmpty(patente.getAreaInvencao())) {
+            this.setSuccess(false);
+            return;
+        }
+        if (StringUtil.getInstance().isNull(patente.getAreaInvencao())) {
+            this.setSuccess(false);
+            return;
+        }
+        // se chegou até aqui, dados aprovados
     }
     
     /**
@@ -45,7 +102,29 @@ public class ServicePatente implements IServicePatente {
      */
     @Override
     public void deletePatente(Patente patente) {
-        
+        this.setSuccess(true);
+        // verifica se o id fornecido é valido.
+        if (patente.getId() < 1) {
+            this.setSuccess(false);
+        }
+        // se chegou até aqui, dados aprovados
+    }
+
+    /**
+     * Returns the success or not of the operation.
+     * @return Boolean.
+     */
+    @Override
+    public boolean patenteSuccess() {
+        return this.success;
+    }
+
+    /**
+     * Only for teste.
+     * @param success 
+     */
+    public void setSuccess(boolean success) {
+        this.success = success;
     }
     
 }
