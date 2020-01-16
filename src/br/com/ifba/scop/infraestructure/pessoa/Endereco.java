e88e63a8ed51d1063da40bcb34289fe67efa526b
beatriz <beatriz@DESKTOP-PNQF1SE>
2020-01-16 13:09:32 -0300

    Criação do pacote br.com.ifba.scop.infraestructure.pessoa criação da classe pessoa //pode ser alterado// criação da classe endereco // pode ser alterado //

---
+++ src/br/com/ifba/scop/infraestructure/pessoa/Endereco.java
@@ -0,0 +1,87 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.ifba.scop.infraestructure.pessoa;
+
+/**
+ *
+ * @author beatriz
+ */
+
+//acrescentar mais coisas
+public class Endereco {
+    private String endereco;
+    private Integer numero;
+    private String complemento;
+    private String bairro;
+    private String cidade;
+    private String estado;
+    private String pais;
+    private Integer cep;
+    
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public Integer getNumero() {
+        return numero;
+    }
+
+    public void setNumero(Integer numero) {
+        this.numero = numero;
+    }
+
+    public String getComplemento() {
+        return complemento;
+    }
+
+    public void setComplemento(String complemento) {
+        this.complemento = complemento;
+    }
+
+    public String getBairro() {
+        return bairro;
+    }
+
+    public void setBairro(String bairro) {
+        this.bairro = bairro;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    public String getPais() {
+        return pais;
+    }
+
+    public void setPais(String pais) {
+        this.pais = pais;
+    }
+
+    public Integer getCep() {
+        return cep;
+    }
+
+    public void setCep(Integer cep) {
+        this.cep = cep;
+    }
+}
--- src/br/com/ifba/scop/infraestructure/pessoa/Pessoa.java
+++ src/br/com/ifba/scop/infraestructure/pessoa/Pessoa.java
@@ -13,17 +13,10 @@ import java.util.Calendar;
  */
 //precisa acrescentar mais coisas :D
 //métodos acessores
-public abstract class Pessoa {
+public abstract class Pessoa{
+    private Endereco endereco;
     private String nome;
     private Integer telefone;
-    private String endereco;
-    private Integer numero;
-    private String complemento;
-    private String bairro;
-    private String cidade;
-    private String estado;
-    private String pais;
-    private Integer cep;
     private Integer cpf;
     private Integer rg;
     private String ocupacao;
@@ -43,70 +36,6 @@ public abstract class Pessoa {
 
     public void setTelefone(Integer telefone) {
         this.telefone = telefone;
-    }
-
-    public String getEndereco() {
-        return endereco;
-    }
-
-    public void setEndereco(String endereco) {
-        this.endereco = endereco;
-    }
-
-    public Integer getNumero() {
-        return numero;
-    }
-
-    public void setNumero(Integer numero) {
-        this.numero = numero;
-    }
-
-    public String getComplemento() {
-        return complemento;
-    }
-
-    public void setComplemento(String complemento) {
-        this.complemento = complemento;
-    }
-
-    public String getBairro() {
-        return bairro;
-    }
-
-    public void setBairro(String bairro) {
-        this.bairro = bairro;
-    }
-
-    public String getCidade() {
-        return cidade;
-    }
-
-    public void setCidade(String cidade) {
-        this.cidade = cidade;
-    }
-
-    public String getEstado() {
-        return estado;
-    }
-
-    public void setEstado(String estado) {
-        this.estado = estado;
-    }
-
-    public String getPais() {
-        return pais;
-    }
-
-    public void setPais(String pais) {
-        this.pais = pais;
-    }
-
-    public Integer getCep() {
-        return cep;
-    }
-
-    public void setCep(Integer cep) {
-        this.cep = cep;
     }
 
     public Integer getCpf() {
@@ -140,4 +69,5 @@ public abstract class Pessoa {
     public void setDataNascimento(Calendar dataNascimento) {
         this.dataNascimento = dataNascimento;
     }
+      
 }
