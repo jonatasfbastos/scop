a4d50e911e2b967507bb3d9e896e5c171bcfe048
mvictor10 <mvictordossantos.limite@gmail.com>
2020-01-17 00:54:55 -0300

    Update IProjetoPesquisaTableModel.java

--- src/br/com/ifba/scop/projetopesquisa/tableModel/IProjetoPesquisaTableModel.java
+++ src/br/com/ifba/scop/projetopesquisa/tableModel/IProjetoPesquisaTableModel.java
@@ -11,16 +11,20 @@ import java.util.List;
 import javax.swing.table.AbstractTableModel;
 
 /**
- *
+ * Classe protótipo com as informações necessárias da tabela.
  * @author mvictor
  */
 public interface IProjetoPesquisaTableModel{
     
+    
+    // lista que irá receber as informações da base de dados.
     public List <ProjetoPesquisa> lista = new ArrayList();
     
     /**
      *
      */
+    
+    //nome das colunas da tabelas.
     public String[] colunas = new String[]
     {
         "Projeto Pesquisa",
