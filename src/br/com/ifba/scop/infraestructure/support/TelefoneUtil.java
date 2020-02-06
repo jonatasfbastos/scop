8f0ead8ac79d0802024361cd7076fc2a033fd7e9
mvictor10 <mvictordossantos.limite@gmail.com>
2020-02-06 13:50:36 -0300

    - validador de números de telefone

--- src/br/com/ifba/scop/infraestructure/support/TelefoneUtil.java
+++ src/br/com/ifba/scop/infraestructure/support/TelefoneUtil.java
@@ -43,4 +43,19 @@ public final class TelefoneUtil {
         return campo.equals("(  )- -    -    ");
     }
     
+    public boolean isTelNumbersInvalid(String campo)
+    {
+        return campo.equals("(00)-0-0000-0000")||
+               campo.equals("(11)-1-1111-1111")||
+               campo.equals("(22)-2-2222-2222")||
+               campo.equals("(33)-3-3333-3333")||
+               campo.equals("(44)-4-4444-4444")||
+               campo.equals("(55)-5-5555-5555")||
+               campo.equals("(666)-6-6666-6666")||
+               campo.equals("(11)-1-1111-1111")||
+               campo.equals("(11)-1-1111-1111")||
+               campo.equals("(11)-1-1111-1111")||
+               campo.length()!= 16;
+    }
+    
 }
