1d38cef5840a3c0377c01932709c45fa11979d42
mvictor10 <mvictordossantos.limite@gmail.com>
2020-02-05 21:11:30 -0300

    - SingleTon para a Validação do CNPJ do Fornecedor. - Atualização - Função para verificar se o número é válido. - Função para verificar se o usuário digitou um sequência de números   repetidos. - Função para verificar se o campo não foi digitado.

--- src/br/com/ifba/scop/infraestructure/support/CnpjUtil.java
+++ src/br/com/ifba/scop/infraestructure/support/CnpjUtil.java
@@ -16,6 +16,8 @@ public class CnpjUtil {
     /**
      * Represents the singleton instance of the class.
      */
+    
+    private static final int[] pesoCNPJ = {6, 5, 4, 3, 2, 9, 8, 7, 6, 5, 4, 3, 2};
     private static CnpjUtil instance;
     private static final String CNPJ_REGEX = "^[\\w-\\+]+(\\.[\\w]+)*@[\\w-]+(\\.[\\w]+)*(\\.[a-z]{2,})$";
     private static Pattern pattern;
@@ -47,10 +49,31 @@ public class CnpjUtil {
         }
         return instance;
     }
-
+    
+    
     public boolean isEmailValid(String Campo)
     {
        matcher = pattern.matcher(Campo);
        return matcher.matches();
+    }
+    
+    //verifica se o campo não está preenchido com números.
+    public boolean isCNPJEmpty(String campo)
+    {
+        return campo.equals("  .   .   /    -  ");
+    }
+    //verifica se o campo do cnpj está com números de sequencia repetidas;
+    public boolean isCPNJNumbersInvalid(String campo)
+    {
+        return  campo.equals("00.000.000/0000-00") ||
+                campo.equals("11.111.111/1111-11") || 
+                campo.equals("22.222.222/2222-22") ||
+                campo.equals("33.333.333/3333-33") ||
+                campo.equals("44.444.444/4444-44") ||
+                campo.equals("55.555.555/5555-55") ||
+                campo.equals("66.666.666/6666-66") ||
+                campo.equals("77.777.777/7777-77") ||
+                campo.equals("88.888.888/8888-88") ||
+                campo.equals("99.999.999/9999-99");
     }
 }
