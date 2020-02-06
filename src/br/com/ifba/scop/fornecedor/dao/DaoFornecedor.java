e05430a5a5517dcd9aa9765fbb27f2ec712d4d84
mvictor10 <mvictordossantos.limite@gmail.com>
2020-02-06 14:39:47 -0300

    - Atualização da classe DaoFornecedor; - Implementação de Métodos para Busca de CNPJ E IE[Inscrição Estadual];

--- src/br/com/ifba/scop/fornecedor/dao/DaoFornecedor.java
+++ src/br/com/ifba/scop/fornecedor/dao/DaoFornecedor.java
@@ -14,10 +14,22 @@ import java.util.List;
  * @author mvictor
  */
 public class DaoFornecedor extends BaseDao<Fornecedor> implements IDaoFornecedor{
-
+    /*
     @Override
     public List<Fornecedor> findByNomeFornecedor(String nome) {
         String query = "select f from fornecedor f WHERE upper(f.nome) like upper('" + nome +"%')";   
+        return BaseDao.entityManager.createQuery(query).getResultList();
+    }
+    */
+    @Override
+    public List<Fornecedor> findByCNPJ(String CNPJ) {
+        String query = "SELECT f FROM fornecedor f WHERE upper(f.cnpj) like upper('"+CNPJ+"%')";
+        return BaseDao.entityManager.createQuery(query).getResultList();
+    }
+
+    @Override
+    public List<Fornecedor> findByIE(String IE) {
+       String query = "SELECT f FROM fornecedor f WHERE upper(f.inscricaoEstadual) like upper('"+IE+"%')";
         return BaseDao.entityManager.createQuery(query).getResultList();
     }
     
